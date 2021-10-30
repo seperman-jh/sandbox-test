201641ce910143262d79f9e07e77a14892f09a8c
seperman.jh <seperman.jh@gmail.com>
2021-10-30 01:29:38 -0700

    Expression test (Part of Destructing - default, rename)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const friend = [1,2,3,4];
 //const sunday = prompt("seventh");
 const weekday = {
-    first : "mon",
+    start : "mon",
+    FIRST : "mon",
     second : "tue",
     third : "wed",
     fourth : "thu",
@@ -9,20 +10,36 @@ const weekday = {
 
 };
 
+const test_default = ({...weekday}) => {
+    const start = {start:"sun"};
+    return {...start, ...weekday};
+    };
 
-console.log({...(false && {weekday})});
+const test_default1 = ({...object}) => {
+    const start = "sun";
+    return {...object, start};
+};
+
+const test_default2 = ({start ="sun", ...weekday}) => ({start, ...weekday});
+
+const test_rename = ({FIRST:first, ...object}) => ({first, ...object});
+
+console.log(test_default(weekday));
+console.log(test_default1(weekday));
+console.log(test_default2(weekday));
+
+console.log(test_rename(weekday));
+
+//test
+// console.log({...(false && {weekday})});
 
 //spread
 //console.log(...friend,5);
 //console.log({...weekday, sixth:"sat"} );
 
 //spread advanced
-
-
 //console.log({...weekday,...(sunday !=="" ? {sunday} : "") });
 // (sunday !=="" ? {sunday} : "") working as ...(sunday !=="" && {sunday}) true && string,object,array ==> string,object,array
-
-
 
 
 //rest
@@ -33,7 +50,3 @@ console.log({...(false && {weekday})});
 // console.log(test_f1(weekday));
 
 
-
-
-
-/*rest*/
